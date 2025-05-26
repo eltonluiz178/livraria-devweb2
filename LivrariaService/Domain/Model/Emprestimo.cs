69b62981351dc42e276035d01a2f175e69eddf0f
eltonluiz178 <eltonluiz533@gmail.com>
2025-05-26 12:39:21 -0300

    feat: Criando entidades (Models) que serão utilizadas no LivrariaService , o serviço para gerenciamento da biblioteca.

---
+++ LivrariaService/Domain/Model/Emprestimo.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Model;
+
+public class Emprestimo
+{
+    public int Id { get; set; }
+    public DateTime DataEmprestimo { get; set; }
+    public DateTime DataDevolucaoPrevista { get; set; }
+    public DateTime DataDevolucaoReal { get; set; }
+    public decimal Multa { get; set; }
+    public string Status { get; set; }
+    public int ClienteId { get; set; }
+    public int LivroId { get; set; }
+}
---
+++ LivrariaService/Domain/Model/Livro.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Model;
+
+public class Livro
+{
+    public int Id { get; set; }
+    public string Título { get; set; }
+    public string Autor { get; set; }
+    public string Descricao { get; set; }
+    public string Genero { get; set; }
+    public string Editora { get; set; }
+    public int QuantidadeDisponivel { get; set; }
+}
