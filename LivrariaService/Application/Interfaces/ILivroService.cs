a513c10f30b41835fc3d34ccb9b55fe08ff9d404
eltonluiz178 <eltonluiz533@gmail.com>
2025-05-26 12:42:27 -0300

    feat: Feito interface e service de Livro, com preparação de dados e primeiras validações de livro

---
+++ LivrariaService/Application/Interfaces/ILivroService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Model;
+
+namespace Application.Interfaces;
+
+public interface ILivroService
+{
+    Task<List<Livro>> BuscarLivrosAsync();
+    Task<Livro> BuscarLivroPorIdAsync(int id);
+    Task<bool> AdicionarLivroAsync(Livro livro);
+    Task<bool> EditarLivroAsync(Livro livro);
+    Task<bool> DeletarLivroAsync(int id);
+}
---
+++ LivrariaService/Application/Services/LivroService.cs
@@ -0,0 +1,103 @@
+﻿using Application.Interfaces;
+using Domain.Model;
+using Infrastructure.Interfaces;
+
+namespace Application.Services;
+
+public class LivroService : ILivroService
+{
+    private readonly ILivroRepository _repository;
+
+    public LivroService(ILivroRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<bool> AdicionarLivroAsync(Livro livro)
+    {
+        try
+        {
+            var resposta = await _repository.AdicionarLivroAsync(livro);
+
+            if(resposta == false)
+            {
+                throw new Exception("Erro ao adicionar o livro, verifique os campos e tente novamente.");
+            }
+
+            return resposta;
+        }
+        catch (Exception e)
+        { throw e; }
+    }
+
+    public async Task<Livro> BuscarLivroPorIdAsync(int id)
+    {
+        try
+        {
+            var livro = await _repository.BuscarLivroPorIdAsync(id);
+
+            if(livro == null)
+            {
+                throw new Exception($"Não foi possível localizar o livro com id {id}.");
+            }
+            return livro;
+        }
+        catch (Exception e)
+        { throw e; }
+    }
+
+    public async Task<List<Livro>> BuscarLivrosAsync()
+    {
+        try
+        {
+            var livros = await _repository.BuscarLivrosAsync();
+
+            if (livros == null)
+            {
+                throw new Exception($"Não foi possível localizar nenhum livro.");
+            }
+            return livros;
+        }
+        catch (Exception e)
+        { throw e; }
+    }
+
+    public async Task<bool> DeletarLivroAsync(int id)
+    {
+        try
+        {
+            var resposta = await _repository.DeletarLivroAsync(id);
+
+            if (resposta == false)
+            {
+                throw new Exception("Erro ao deletar o livro.");
+            }
+
+            return resposta;
+        }
+        catch (Exception e)
+        { throw e; }
+    }
+
+    public async Task<bool> EditarLivroAsync(Livro livro)
+    {
+        try
+        {
+            if (await _repository.BuscarLivroPorIdAsync(livro.Id) == null)
+            {
+                throw new Exception($"Não foi possível localizar o livro com id {livro.Id}.");
+            }
+
+            var resposta = await _repository.EditarLivroAsync(livro);
+
+            if (resposta == false)
+            {
+                throw new Exception("Erro ao editar o livro, verifique os campos e tente novamente.");
+            }
+
+            return resposta;
+        }
+        catch (Exception e)
+        { throw e; }
+    }
+}
