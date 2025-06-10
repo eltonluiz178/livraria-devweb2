95344802b65e483606f56033c79187a3b12308b8
eltonluiz178 <eltonluiz533@gmail.com>
2025-06-09 21:04:25 -0300

    feat: Program.cs com conexão com banco , Cors entre outros

--- LivrariaService/LivrariaService/Program.cs
+++ LivrariaService/LivrariaService/Program.cs
@@ -1,6 +1,34 @@
+using LivrariaService.Config;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+});
+
 // Add services to the container.
+
+string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddScoped<IDbConnection>(provider =>
+{
+    SqlConnection connection = new SqlConnection(connectionString);
+    connection.Open();
+    return connection;
+});
+
+builder.Services.Injection(builder.Configuration);
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +43,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
