a7013121ae200c79b462bf6d39453ece7a1c53a3
eltonluiz178 <eltonluiz533@gmail.com>
2025-06-09 21:02:57 -0300

    feat: Cria pasta config com Profile para mapeamento do automapper e Injeção de depedências

---
+++ LivrariaService/LivrariaService/Config/AutoMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Domain.Dtos;
+using Domain.Model;
+
+namespace LivrariaService.Config;
+
+public class AutoMapper: Profile
+{
+    public AutoMapper()
+    {
+        CreateMap<CriarEmprestimoDto, Emprestimo>().ReverseMap();
+    }
+}
---
+++ LivrariaService/LivrariaService/Config/DependecyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Infrastructure.Interfaces;
+using Infrastructure.Repositories;
+
+namespace LivrariaService.Config;
+
+public static class DependecyInjection
+{
+    public static IServiceCollection Injection(this IServiceCollection service, IConfiguration configuration)
+    {
+        service.AddScoped<ILivroRepository, LivroRepository>();
+        service.AddScoped<ILivroService, LivroService>();
+
+        service.AddScoped<IClienteRepository, ClienteRepository>();
+        service.AddScoped<IClienteService, ClienteService>();
+
+        service.AddScoped<IEmprestimoRepository, EmprestimoRepository>();
+        service.AddScoped<IEmprestimoService, EmprestimoService>();
+
+        service.AddScoped<INotificador, Notificador>();
+
+        return service;
+    }
+}
