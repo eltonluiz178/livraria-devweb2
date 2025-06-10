6478412adba6b9ffc61c4e3da682dbd88b6ab795
eltonluiz178 <eltonluiz533@gmail.com>
2025-06-09 22:03:16 -0300

    build: Subir docker

--- LivrariaService/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ LivrariaService/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3c6050f193d7c418515eccff3c56e06a472c525")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a67f0666c1038c0143cb71362f4b8bd1adaed13e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
